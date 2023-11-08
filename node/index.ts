274e3de3b0d7ccc9531d9ee7be4a0ca08b05ee83
Tiago de Andrade Freire <freire.tiago@gmail.com>
2023-11-08 12:23:52 -0300

    fix: node clients config

--- node/index.ts
+++ node/index.ts
@@ -1,20 +1,25 @@
-import type { ClientsConfig } from '@vtex/api'
-import { LRUCache, method, Service } from '@vtex/api'
+import type { Cached, ClientsConfig } from '@vtex/api'
+import { LRUCache, Service, method } from '@vtex/api'
 
 import { Clients } from './clients'
 import { getFixedPrices as getFixedPricesMiddleware } from './handlers/getFixedPrices'
 import { getPromotions } from './handlers/getPromotions'
 import { getFixedPrices } from './resolvers/getFixedPrices'
 
-const TIMEOUT_MS = 10000
-const memoryCache = new LRUCache<string, never>({ max: 5000 })
+const TIMEOUT_MS = 3000
+const CONCURRENCY = 10
+const memoryCache = new LRUCache<string, Cached>({ max: 5000 })
 
 const clients: ClientsConfig<Clients> = {
   implementation: Clients,
   options: {
     default: {
-      retries: 2,
+      exponentialTimeoutCoefficient: 2,
+      exponentialBackoffCoefficient: 2,
+      initialBackoffDelay: 100,
+      retries: 10,
       timeout: TIMEOUT_MS,
+      concurrency: CONCURRENCY,
       memoryCache,
     },
   },
